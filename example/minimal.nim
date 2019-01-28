1b772a116ded5a1180d1fe3e4fb9fe05f1da69c1
juancarlospaco <juancarlospaco@gmail.com>
2019-01-28 11:57:47 -0300

    Minimal example 2

---
+++ example/minimal.nim
@@ -0,0 +1,22 @@
+import nimterlingua
+nimterlingua("translations.cfg")
+echo [
+  "Is this the real life?",
+  "Is this just fantasy?",
+  "Caught in a landslide",
+  "No escape from reality",
+  "Open your eyes",
+  "Look up to the skies and see",
+  "I'm just a poor boy, I need no sympathy",
+  "Because I'm easy come, easy go",
+  "A little high, little low",
+  "Anyway the wind blows, doesn't really matter to me, to me",
+  "Mama, just killed a man",
+  "Put a gun against his head",
+  "Pulled my trigger, now he's dead",
+  "Mama, life had just begun",
+  "But now I've gone and thrown it all away",
+  "Mama, ooo, Didn't mean to make you cry",
+  "If I'm not back again this time tomorrow",
+  "Carry on, carry on, as if nothing really matters",
+]
